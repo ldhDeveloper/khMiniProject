eb891738de23592c3643714a0fc2b3f31fab5899
jiseung <jiseung@192.168.50.45>
2017-01-04 17:06:57 +0900

    배경색 흰색으로 하려고 패널을 프레임에 추가했었는데 그렇게 하니까 자꾸 특정 하얀 네모가 생기길래 주석처리하고 비행기 기능 간단하게 추가 & 전체적으로 코드 수정. 맥북으로 해서 윈도우에서 배치가 제대로 보일지 모르겠음  -지승

--- miniProject/src/gui/test/TestGui.java
+++ miniProject/src/gui/test/TestGui.java
@@ -5,15 +5,19 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class TestGui extends JFrame{
+public class TestGui extends JFrame implements ActionListener, MouseListener {
 	
 	Charcter c;
 	JFrame f = new JFrame();
+	JPanel p = new JPanel();
 	JLabel car, carinfo, building, diceLabel;
 	JLabel diceLabelImage1, diceLabelImage2;
+	ImageIcon diceImage[] ;
+	JLabel planeMsg = new JLabel("이동할 도시를 선택해주세요 ", JLabel.CENTER);
 	JButton btn1;
 	JLabel[] Jlist = new JLabel[24];
 	Cities[] ct = new Cities[24];
+	int location, xPoint, yPoint ;
 	
 	
 	public TestGui(){ 
@@ -41,9 +45,12 @@ public class TestGui extends JFrame{
 		f.setTitle("부루마블");
 		f.setSize(1000, 780);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
 		f.setLayout(null);
+		
+		p.setSize(f.getSize());
+		p.setOpaque(true);
+		p.setBackground(Color.white);
+		p.setLayout(null);
 		
 		car = new JLabel("A");
 		car.setOpaque(true);
@@ -88,9 +95,11 @@ public class TestGui extends JFrame{
 		Jlist[5].setSize(100, 50);
 		Jlist[5].setBorder(BorderFactory.createLineBorder(Color.black, 1));
 		
-		Jlist[6] = new JLabel("★");
+		Jlist[6] = new JLabel(" ★비행기★ ");
 		Jlist[6].setLocation(710, 50);
 		Jlist[6].setSize(100, 50);
+		Jlist[6].setOpaque(true);
+		Jlist[6].setBackground(Color.cyan);
 		Jlist[6].setBorder(BorderFactory.createLineBorder(Color.black, 1));
 		
 		Jlist[7] = new JLabel(" " + ct[7].getName());
@@ -148,9 +157,12 @@ public class TestGui extends JFrame{
 		Jlist[17].setSize(100, 50);
 		Jlist[17].setBorder(BorderFactory.createLineBorder(Color.black, 1));
 		
-		Jlist[18] = new JLabel("★");
+		Jlist[18] = new JLabel(" ★무인도★ ");
 		Jlist[18].setLocation(50, 410);
 		Jlist[18].setSize(100, 50);
+		Jlist[18].setOpaque(true);
+		Jlist[18].setBackground(Color.darkGray);
+		Jlist[18].setForeground(Color.white);
 		Jlist[18].setBorder(BorderFactory.createLineBorder(Color.black, 1));
 		
 		Jlist[19] = new JLabel(" " + ct[19].getName());
@@ -194,7 +206,7 @@ public class TestGui extends JFrame{
 			
 		});*/
 
-		ImageIcon diceImage[] = new ImageIcon[6];
+		diceImage = new ImageIcon[6];
 		for (int i=0 ; i<6 ; i++) 
 			diceImage[i] = new ImageIcon("dice" + (i+1) + ".png");
 		
@@ -215,59 +227,41 @@ public class TestGui extends JFrame{
 		btn1.setSize(100, 50);
 		btn1.setBorder(BorderFactory.createLineBorder(Color.black, 1));
 		
-
+		btn1.addActionListener(this);
 		
-		btn1.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				int dice1 = new Random().nextInt(6) + 1;
-				int dice2 = new Random().nextInt(6) + 1;
-				int dice = dice1 + dice2;
-				
-				diceLabel.setText(dice + "칸 이동! ");
-				
-				diceLabelImage1.setIcon(diceImage[dice1-1]);
-				diceLabelImage2.setIcon(diceImage[dice2-1]);
-
-				int location = c.getLocation() + dice;
-				
-				if(location >= Jlist.length){
-					location -= Jlist.length;
-				}
-				
-				c.setLocation(location); // 캐릭터 위치 값 저장
-				
-				int xPoint = (int)Jlist[location].getLocation().getX() + 50;
-				int yPoint = (int)Jlist[location].getLocation().getY() + 18;
-				car.setLocation(xPoint, yPoint); // 말 위치 이동
-				
-				if(ct[location] != null){
-					cityAction(location);
-					carinfo.setText("이름 : " + c.getName()
-					+ " 자금 : " + c.getMoney());
-				}
-			}
-
-			
-		});
+		add();
 		
+	}
 	
+	public void add() {
 		
 		for(int i =0; i<Jlist.length ; i++){
+			if (!(i==6)) // 비행기가 아니면 
+				Jlist[i].addMouseListener(this);
 			f.add(Jlist[i]);
 		}
 		
-		
+		/*
+		p.add(car);
+		p.add(carinfo);
+		p.add(btn1);
+		p.add(diceLabel);
+		p.add(diceLabelImage1);
+		p.add(diceLabelImage2);
+		p.add(planeMsg, 0);
+		f.add(p);
+		*/
+
 		f.add(car);
 		f.add(carinfo);
 		f.add(btn1);
 		f.add(diceLabel);
 		f.add(diceLabelImage1);
 		f.add(diceLabelImage2);
+		f.add(planeMsg, 0);
 		
+		planeMsg.setVisible(false);
 		f.setVisible(true);
-		
 	}
 	
 	public void cityAction(int location) {
@@ -279,19 +273,127 @@ public class TestGui extends JFrame{
 			
 			if ((result = new CityManager().
 					OwnCity(ct[location], c)) == 0) {
-				building = new JLabel();
-				building.setSize(30,30);
-				building.setLocation(Jlist[location].getLocation());
-				building.setOpaque(true);
-				building.setBackground(Color.blue);
-				building.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-				f.add(building);
+				building(location);
 			}
 		}
 		
 	}
+	
+	public void building(int location) {
+		building = new JLabel();
+		building.setSize(30,30);
+		building.setLocation(Jlist[location].getLocation());
+		building.setOpaque(true);
+		building.setBackground(Color.blue);
+		building.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+		f.add(building);
+	}
+	
+	public void plane() {
+		btn1.setEnabled(false);
+		planeMsg.setSize(400, 150);
+		planeMsg.setLocation(f.getSize().width/2 - 400/2 , f.getSize().height/2 - 150);
+		planeMsg.setOpaque(true);
+		planeMsg.setBackground(new Color(225, 225, 225));
+		planeMsg.setVisible(true);
+	}
+	
+	public void moveHorse(int location) {
+		
+		c.setLocation(location); // 캐릭터 위치 값 저장
+		
+		xPoint = (int)Jlist[location].getLocation().getX() + 50;
+		yPoint = (int)Jlist[location].getLocation().getY() + 18;
+		car.setLocation(xPoint, yPoint); // 말 위치 이동
+		
+		if(ct[location] != null){
+			cityAction(location);
+			carinfo.setText("이름 : " + c.getName()
+			+ " 자금 : " + c.getMoney());
+		}
+		else
+			switch (location) {
+			case 0 :
+				System.out.println("월급 ");
+				break;
+			case 3 :
+				System.out.println("★");
+				break;
+			case 6:
+				plane();
+				break;
+			case 9 :
+				System.out.println("★");
+				break;
+			case 12 :
+				System.out.println("★");
+				break;
+			case 15 :
+				System.out.println("★");
+				break;
+			case 18 :
+				System.out.println("무인도 ");
+				break;
+			case 21 :
+				System.out.println("★");
+				break;
+			}
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+			
+			int dice1 = new Random().nextInt(6) + 1;
+			int dice2 = new Random().nextInt(6) + 1;
+			int dice = dice1 + dice2;
+			
+			diceLabel.setText(dice + "칸 이동! ");
+			
+			diceLabelImage1.setIcon(diceImage[dice1-1]);
+			diceLabelImage2.setIcon(diceImage[dice2-1]);
+
+			location = c.getLocation() + dice;
+			
+			if(location >= Jlist.length)
+				location -= Jlist.length;
+			
+			moveHorse(location);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+		JLabel selectedCountry = (JLabel)(e.getSource());
+		
+		if (planeMsg.isVisible()) {
+			for (int i=0 ; i<Jlist.length ; i++)
+				if (Jlist[i] == selectedCountry) {
+					moveHorse(i);
+					planeMsg.setVisible(false);
+					btn1.setEnabled(true);
+					break;
+				}
+		}
+			
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	public static void main(String[] args) {
 		TestGui test = new TestGui();
 	}
+
 }
