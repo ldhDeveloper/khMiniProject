feb8f9a8bcef8b8fd5a75ef34ebbe2d12401c31a
성혁 <tjdvkf19@gmail.com>
2017-01-05 15:07:36 +0900

    화면 담을 메인프레임

---
+++ miniProject/src/gui/mvc/view/MainFrame.java
@@ -0,0 +1,16 @@
+package gui.mvc.view;
+import javax.swing.*;
+import java.awt.*;
+public class MainFrame extends JFrame{
+	public MainFrame(){
+		this.setTitle("Blue Marble");
+		this.setSize(new Dimension(1200, 800));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+		
+		this.setVisible(true);
+		
+	}
+	
+
+}
