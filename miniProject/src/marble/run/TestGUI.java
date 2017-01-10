03b685e7a3f02a6b45dbc10cb1f2ad48431d4113
ldhDeveloper <ldh8206@gmail.com>
2017-01-10 10:47:21 +0900

    임시동작 가능

--- miniProject/src/marble/run/TestGUI.java
+++ miniProject/src/marble/run/TestGUI.java
@@ -8,17 +8,20 @@ import marble.view.*;
 public class TestGUI {
 
 	public static void main(String[] args) {
-		try {
+		/*try {
 			ClientBackground client = new ClientBackground();
 			client.connet();
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 		//new PageLogIn();
 		//new PageJoin();
 		//new PageGame();
+		MainFrame m = new MainFrame();
+		m.getCardLayout().show(m.getContentPane(), "game");
+		m.represent();
 	}
 
 }
